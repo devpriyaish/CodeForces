466de6a98941db0cc0e8779bd257cd0016c68d26
Devpriya Shivani <dpshiv12@gmail.com>
2019-11-06 20:52:25 +0530

    Add files via upload

---
+++ A - Domino piling.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args)
+    {
+        Scanner reader = new Scanner(System.in);
+        int n = reader.nextInt();
+        int m = reader.nextInt();
+        System.out.println(n*m/2);
+    }
+}
